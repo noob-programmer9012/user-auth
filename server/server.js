da43ae52460ed44b2b87d1bd295247ce43614604
Parth Patel <parth.sclub@gmail.com>
2023-03-03 22:23:29 +0530

    fixed typo

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ async function main () {
   app.use('/api/dashboard', dashboardRoute)
   app.use('/api/firm', firmRoute)
   app.use('/api/ledgers', ledgerRoutes)
-  app.use('/api/trasnporter', transporterRoutes)
+  app.use('/api/transporter', transporterRoutes)
+
   app.use('*', (req, res, next) => {
     res.status(404).json({ error: 'Path Not Found' })
   })
