bbc5f9a01663cbcf1e120e2896b08c6239c4b0a2
noob-programmer9012 <parth.sclub@gmail.com>
2023-03-07 17:44:40 +0530

    transporter data

--- client/src/Components/TransporterData.js
+++ client/src/Components/TransporterData.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Axios from 'axios'
 import { styled } from '@mui/material/styles'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
@@ -6,15 +7,18 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
+
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import { IconButton } from '@mui/material'
 
+import UserContext from '../Context/UserContext'
+
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
+    color: theme.palette.common.black,
+    backgroundColor: theme.palette.common.white,
+    boxShadow: '5px 5px 10px #c2c2c2'
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14
@@ -35,34 +39,74 @@ function createData (id, transporterName, gstNumber) {
   return { id, transporterName, gstNumber }
 }
 
-const rows = [
-  createData(1, 'Maruti Motor', '24AQQPN6533K1ZC'),
-  createData(2, 'Janvi Logistics', '24DAQPS2504L1ZC'),
-  createData(3, 'Pavan goods freight carrier', '24ACVPS7030F1ZC')
-]
-
 export default function TransporterData () {
+  const { serverUrl, firm } = React.useContext(UserContext)
+  const [transporters, setTransporters] = React.useState(null)
+  const firmId = JSON.parse(firm)._id
+  React.useEffect(() => {
+    const transporters = async () => {
+      let token = localStorage.getItem('authToken')
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+      try {
+        const data = await Axios.get(
+          `${serverUrl}/api/transporter/gettransporters/${firmId}`,
+          config
+        )
+        setTransporters(JSON.stringify(data.data.data))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    transporters()
+  })
+
+  transporters && console.log(transporters)
+
+  const rows = [
+    createData(1, 'Maruti Motor', '24AQQPN6533K1ZC'),
+    createData(2, 'Janvi Logistics', '24DAQPS2504L1ZC'),
+    createData(3, 'Pavan goods freight carrier', '24ACVPS7030F1ZC')
+  ]
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: '300px' }} aria-label='customized table'>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>ID</StyledTableCell>
-            <StyledTableCell>Transporter Name</StyledTableCell>
-            <StyledTableCell>GST Number&nbsp;</StyledTableCell>
-            <StyledTableCell>Actions&nbsp;</StyledTableCell>
+    <TableContainer>
+      <Table aria-label='customized table'>
+        <TableHead sx={{ p: 1 }}>
+          <TableRow sx={{ p: 1 }}>
+            <StyledTableCell sx={{ p: 1 }}>ID</StyledTableCell>
+            <StyledTableCell sx={{ p: 1 }}>Transporter Name</StyledTableCell>
+            <StyledTableCell sx={{ p: 1 }}>GST Number&nbsp;</StyledTableCell>
+            <StyledTableCell sx={{ p: 1 }}>Actions&nbsp;</StyledTableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
+        <TableBody sx={{ p: 1 }}>
           {rows.map(row => (
-            <StyledTableRow key={row.id}>
-              <StyledTableCell component='th' scope='row'>
+            <StyledTableRow
+              key={row.id}
+              id={row.transporterName}
+              sx={{ p: 1 }}
+              className='transporters'
+            >
+              <StyledTableCell component='th' scope='row' sx={{ p: 1 }}>
                 {row.id}
               </StyledTableCell>
-              <StyledTableCell>{row.transporterName}</StyledTableCell>
-              <StyledTableCell>{row.gstNumber}</StyledTableCell>
+              <StyledTableCell sx={{ p: 1 }}>
+                {row.transporterName}
+              </StyledTableCell>
+              <StyledTableCell sx={{ p: 1 }}>{row.gstNumber}</StyledTableCell>
               <StyledTableCell
-                sx={{ display: 'flex', alignItems: 'center', ml: -2 }}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  ml: -2,
+                  p: 1,
+                  m: 0
+                }}
               >
                 <IconButton aria-label='delete' size='large'>
                   <DeleteIcon sx={{ color: '#D32F2F' }} />
--- client/src/Pages/TransporterPage.js
+++ client/src/Pages/TransporterPage.js
@@ -1,11 +1,55 @@
-import { Box } from '@mui/material'
+import { Grid, IconButton, InputBase, Paper } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
 
 import TransporterData from '../Components/TransporterData'
 
 export default function TransporterPage () {
+  function CustomizedInputBase () {
+    function handleSearch (e) {
+      const transporters = document.getElementsByClassName('transporters')
+      for (let transporter of transporters) {
+        if (
+          transporter.id.toUpperCase().indexOf(e.target.value.toUpperCase()) !==
+          -1
+        ) {
+          transporter.style.display = ''
+        } else {
+          transporter.style.display = 'none'
+        }
+      }
+    }
+
+    return (
+      <Paper
+        component='form'
+        sx={{
+          p: '2px 4px',
+          display: 'flex',
+          alignItems: 'center',
+          maxHeight: '48px',
+          alignSelf: 'center'
+        }}
+      >
+        <IconButton sx={{ p: '10px' }} aria-label='menu'>
+          <SearchIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder='Search Transporters'
+          inputProps={{ 'aria-label': 'search transporters' }}
+          onChange={handleSearch}
+        />
+      </Paper>
+    )
+  }
   return (
-    <Box sx={{ p: 2 }}>
-      <TransporterData />
-    </Box>
+    <Grid container sx={{ pl: 1, pr: 1, pt: 2 }}>
+      <Grid item xs={12} sx={{ pb: 1 }}>
+        <CustomizedInputBase />
+      </Grid>
+      <Grid item xs={12}>
+        <TransporterData />
+      </Grid>
+    </Grid>
   )
 }
