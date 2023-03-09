ccd91a5c9721df45fe351d41c8789092ee14e3f1
noob-programmer9012 <parth.sclub@gmail.com>
2023-03-09 14:25:21 +0530

    tranporter edit and delete controller

--- server/controller/transporterController.js
+++ server/controller/transporterController.js
@@ -47,3 +47,56 @@ export const gettransporters = async (req, res, next) => {
     next(error)
   }
 }
+
+export async function editTransporter (req, res, next) {
+  const { transporterId, firmId } = req.params
+  const transporter = await Transporter.findOne({ _id: transporterId })
+  if (transporter) {
+    try {
+      const { transporterName, gst_no } = req.body
+      if (!transporterName || !gst_no) {
+        return next(new ErrorResponse('Both fields are mandatory!', 400))
+      }
+      if (
+        transporterName.toUpperCase() === transporter.transporterName &&
+        gst_no.toUpperCase() === transporter.gst_no
+      ) {
+        return next(new ErrorResponse('No Changes were made', 400))
+      }
+      if (
+        (await Transporter.findOne({ firmId, transporterName })) &&
+        Transporter.findOne({ firmId, gst_no })
+      ) {
+        return next(new ErrorResponse('Duplicate Entry', 400))
+      }
+      transporter.transporterName = transporterName
+      transporter.gst_no = gst_no
+      await transporter.save()
+      res.status(200).json({
+        success: true,
+        data: transporter
+      })
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    return next(new ErrorResponse('No Transporter Found', 404))
+  }
+}
+
+export async function deleteTransporter (req, res, next) {
+  const { transporterId } = req.params
+  try {
+    const transporter = await Transporter.findOne({ _id: transporterId })
+    if (!transporter) {
+      return next(new ErrorResponse('No Transporter Found', 404))
+    }
+    await Transporter.deleteOne({ _id: req.params.transporterId })
+    res.status(200).json({
+      success: true,
+      message: 'Successfully deleted transporter'
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- server/routes/transporter.js
+++ server/routes/transporter.js
@@ -3,12 +3,16 @@ import express from 'express'
 import { protect } from '../middleware/auth.js'
 import {
   addtransporter,
-  gettransporters
+  gettransporters,
+  editTransporter,
+  deleteTransporter
 } from '../controller/transporterController.js'
 
 const router = express.Router()
 
 router.route('/addtransporter').post(protect, addtransporter)
 router.route('/gettransporters/:firmId').get(protect, gettransporters)
+router.route('/edit/:transporterId/:firmId').put(protect, editTransporter)
+router.route('/delete/:transporterId').delete(protect, deleteTransporter)
 
 export default router
