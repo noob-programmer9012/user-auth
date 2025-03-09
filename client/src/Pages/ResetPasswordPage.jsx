0f01957a9be63fd6f1857f8a925a1523b06e54b2
noob-programmer9012 <parth.sclub@gmail.com>
2025-03-09 15:50:20 +0530

    reset password working

--- client/src/Pages/ResetPasswordPage.jsx
+++ client/src/Pages/ResetPasswordPage.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
@@ -7,7 +7,9 @@ import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
-// import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
 import {
   Alert,
   CircularProgress,
@@ -22,14 +24,17 @@ import {
 
 import UserContext from '../Context/UserContext'
 
-export default function LoginPage() {
-  const { serverUrl } = React.useContext(UserContext)
-  const [password, setPassword] = React.useState(null)
+export default function ResetPasswordPage() {
+  const { serverUrl } = React.useContext(UserContext);
+  const [password, setPassword] = React.useState('')
+  const [match, setMatch] = React.useState('');
   const [show, setShow] = React.useState(false)
   const [error, setError] = React.useState('')
   const [progress, setProgress] = React.useState(false)
-
   const [showPassword, setShowPassword] = React.useState(false)
+
+  const token = useParams();
+  const navigate = useNavigate();
 
   const handleClickShowPassword = () => setShowPassword(show => !show)
 
@@ -38,6 +43,32 @@ export default function LoginPage() {
   }
 
   const handleSubmit = async event => {
+    event.preventDefault();
+    if (!password || !match) {
+      setError("Please enter password");
+      setShow(true);
+    }
+
+    if ((password && match) && password !== match) {
+      setError("Passwords do not match!");
+      setShow(true);
+    }
+
+    const url = `${serverUrl}/api/user/resetpassword/${token.resetToken}`;
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    }
+
+    try {
+      const { data } = await axios.put(url, { password }, config);
+      if (data.success) navigate("/login");
+    } catch (error) {
+      setError(error.message)
+      setShow(true)
+    }
   }
 
   return (
@@ -80,6 +111,7 @@ export default function LoginPage() {
               id='outlined-adornment-password'
               type={showPassword ? 'text' : 'password'}
               autoComplete='current-password'
+              value={password}
               onChange={e => {
                 setPassword(e.target.value)
                 setShow(false)
@@ -103,6 +135,11 @@ export default function LoginPage() {
               label="Retype Password"
               type="password"
               sx={{ mt: 2 }}
+              value={match}
+              onChange={e => {
+                setMatch(e.target.value);
+                setShow(false);
+              }}
             />
           </FormControl>
           <Button
